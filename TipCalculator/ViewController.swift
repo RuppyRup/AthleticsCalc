ad81894ca66d4a92d918ed27298374b57f3692b3
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-03-11 17:31:39 +0000

    Most of interface added

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
+    @IBOutlet weak var tipPercentageLbl: UILabel!
+    @IBOutlet weak var tipLbl: UILabel!
+    @IBOutlet weak var totalLbl: UILabel!
+    
+    // create an instance of tipModel
+    var tip = Tipmodel(billAmount: 0.0, tipPercent: 0.0)
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        func setTipCalculationValues () {
+            tip.tipPercent = Double(tipPercentageSlider.value)
+            tip.billAmount = Double(textField.text!)!
+            tip.calculateTip()
+        }
+        
+        func updateUI() {
+            tipLbl.text = String(format: "£%0.2f", tip.tipAmount)
+            totalLbl.text = String(format: "£%0.2f", tip.totalAmount)
+            tipPercentageLbl.text = "Tip: \(Int(tipPercentageSlider.value * 100)) %"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func billAmountDidChange(_ sender: Any) {
+        print("Bill amount has changed")
     }
-
-
+    
+    @IBAction func tipPercentDidChange(_ sender: Any) {
+        print(tipPercentageSlider.value)
+    }
 }
 
