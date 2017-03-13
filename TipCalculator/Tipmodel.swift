3db4a56f373130b8946f26a86be3aa01fdd47103
RuppyRup <rupert.waldron@yahoo.co.uk>
2017-03-13 13:37:31 +0000

    Added bill split slider

--- TipCalculator/Tipmodel.swift
+++ TipCalculator/Tipmodel.swift
@@ -14,6 +14,8 @@ class Tipmodel {
     private var _tipPercent: Double = 0
     private var _tipAmount: Double = 0
     private var _totalAmount: Double = 0
+    private var _numberOfDinners: Int = 1
+    private var _splitAmount: Double = 0
     
     // use getters and setters to access the private variables
     var billAmount: Double {
@@ -43,15 +45,40 @@ class Tipmodel {
             return _totalAmount
         }
     }
+    
+    var numberOfDinners: Int {
+        get {
+            return _numberOfDinners
+        } set {
+            _numberOfDinners = newValue
+        }
+    }
+    
+    var splitAmount: Double {
+        get {
+            return _splitAmount
+        }
+    }
+
     // write the customer intialiser
-    init(billAmount: Double, tipPercent: Double) {
+    init(billAmount: Double, tipPercent: Double, numberOfDinners: Int) {
         self._billAmount = billAmount
         self._tipPercent = tipPercent
+        self._numberOfDinners = numberOfDinners
     }
     
     //create function to calculate the tip
     func calculateTip() {
         _tipAmount = billAmount * tipPercent
         _totalAmount = billAmount + tipAmount
+    }
+    
+    //create function to calculate the split
+    func calculateSplit() {
+        if numberOfDinners > 0 {
+            _splitAmount = totalAmount / Double(numberOfDinners)
+        } else {
+            _splitAmount = totalAmount
+        }
     }
 }
--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -15,9 +15,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercentageLbl: UILabel!
     @IBOutlet weak var tipLbl: UILabel!
     @IBOutlet weak var totalLbl: UILabel!
+    var paySplitSlider: Int = 4
+    @IBOutlet weak var splitNumberLbl: UILabel!
+    @IBOutlet weak var youPayLbl: UILabel!
     
+    @IBOutlet weak var testSlider: UISlider!
     // create an instance of tipModel
-    var tip = Tipmodel(billAmount: 0.0, tipPercent: 0.0)
+    var tip = Tipmodel(billAmount: 0.0, tipPercent: 0.0, numberOfDinners: 1)
     
 
     override func viewDidLoad() {
@@ -30,12 +34,17 @@ class ViewController: UIViewController {
         tipLbl.text = String(format: "£%0.2f", tip.tipAmount)
         totalLbl.text = String(format: "£%0.2f", tip.totalAmount)
         tipPercentageLbl.text = "Tip: \(Int(tipPercentageSlider.value * 100)) %"
+        splitNumberLbl.text = "\(Int(round(testSlider.value))) Way Split"
+        //splitNumberLbl.text = "\(Int(paySplitSlider.value)) Way Split"
+        youPayLbl.text = String(format: "£%0.2f", tip.splitAmount)
     }
 
     func setTipCalculationValues () {
         tip.tipPercent = Double(tipPercentageSlider.value)
         tip.billAmount = ((textField.text)! as NSString).doubleValue
+        tip.numberOfDinners = Int(round(testSlider.value))
         tip.calculateTip()
+        tip.calculateSplit()
     }
 
     
@@ -49,8 +58,17 @@ class ViewController: UIViewController {
         let roundedValue = round(sender.value * steps) / steps
         sender.value = roundedValue
         print(tipPercentageSlider.value)
+        //(round(testSlider.value))
         setTipCalculationValues ()
         updateUI()
     }
+    
+    @IBAction func splitSliderDidChange(_ sender: Any) {
+        //print(tipPercentageSlider.value)
+        print(round(testSlider.value))
+        setTipCalculationValues ()
+        updateUI()
+    }
+    
 }
 
